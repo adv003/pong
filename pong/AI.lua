d9ffb76cfa5e7b6feb536892d43e10661d915b6e
adv003 <advay03@gmail.com>
2023-07-12 23:23:01 +0530

    made file AI.lua and implemented ai logic

---
+++ pong/AI.lua
@@ -0,0 +1,40 @@
+local AI = {}
+
+function AI:init()
+    -- Initialize AI properties and variables here
+    self.paddle = Paddle(10, VIRTUAL_HEIGHT / 2 - 20, 5, 40) -- Assuming player1 is the AI-controlled paddle
+    self.ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4) -- Assuming the ball is accessible from the AI player
+end
+
+function AI:update(dt)
+    -- Update AI logic here
+    self:movePaddle()
+end
+
+function AI:movePaddle()
+    -- Move the AI paddle based on the ball's position and the AI's strategy
+    
+    -- Get the current Y position of the AI paddle
+    local paddleY = self.paddle.y
+
+    -- Get the current Y position of the ball
+    local ballY = self.ball.y
+
+    -- Adjust the paddle's Y position based on the ball's Y position
+    if ballY < paddleY then
+        -- Move paddle up
+        paddleY = paddleY - PADDLE_SPEED * dt -- Adjust this line if you have a different paddle speed variable name
+    elseif ballY > paddleY then
+        -- Move paddle down
+        paddleY = paddleY + PADDLE_SPEED * dt -- Adjust this line if you have a different paddle speed variable name
+    end
+
+    -- Ensure the paddle remains within the game screen boundaries
+    paddleY = math.max(paddleY, 0)
+    paddleY = math.min(paddleY, VIRTUAL_HEIGHT - self.paddle.height)
+
+    -- Update the paddle's Y position
+    self.paddle.y = paddleY
+end
+
+return AI
